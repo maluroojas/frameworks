f8666f88d479da72df57282cba15730e85a94c2d
maluroojas <marialuisarojas666@gmail.com>
2023-04-06 14:46:41 -0400

    aula 06/04

--- my-app/src/app/components/login/login.component.ts
+++ my-app/src/app/components/login/login.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 isError = false;
+isSucess = false;
+
+user = ""
+pwd = ""
+
+login(){
+console.log("User= " +this.user + "Password =" +this.pwd)
+if (this.user == "maluroojas" && this.pwd == "123") {
+  this.isError = false;
+  this.isSucess = true
+  console.log ("Login aprovado")
+} 
+
+else {
+  this.isError = true;
+  this.isSucess = false;
 }
+}
+}
+
+
