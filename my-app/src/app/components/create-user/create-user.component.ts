fb04d4c67f0fd327b58c46b713a50c5d6447a8ca
maluroojas <marialuisarojas666@gmail.com>
2023-05-03 12:19:24 -0400

    codigo final

--- my-app/src/app/components/create-user/create-user.component.ts
+++ my-app/src/app/components/create-user/create-user.component.ts
@@ -3,26 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
-  styleUrls: ['./create-user.component.css']
+  styleUrls: ['./create-user.component.css'],
 })
 export class CreateUserComponent {
   isError = false;
   isSucess = false;
-  
-  user = ""
-  pwd = ""
-  
-  login(){
-  console.log("User= " +this.user + "Password =" +this.pwd)
-  if (this.user == "maluroojas" && this.pwd == "123") {
-    this.isError = false;
-    this.isSucess = true
-    console.log ("Login aprovado")
-  } 
-  
-  else {
-    this.isError = true;
-    this.isSucess = false;
-  }
+
+  user = '';
+  pwd = '';
+  double = '';
+  login() {
+    console.log('user = ' + this.user + 'Password = ' + this.pwd);
+    if (this.pwd === this.double) {
+      console.log('login aprovado');
+      this.isError = false;
+      this.isSucess = true;
+    } else {
+      this.isError = true;
+      this.isSucess = false;
+    }
   }
 }
