d9ffdb1c5024e5fb2923e4229d2731cff2ea91fd
maluroojas <marialuisarojas666@gmail.com>
2023-05-29 16:54:08 -0400

    aula 29/05

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroComponent } from './compnents/hero/hero.component';
 import { LoginComponent } from './components/login/login.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { CreateUserComponent } from './components/create-user/create-user.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
@@ -28,7 +28,8 @@ import { TodoListComponent } from './components/todo-list/todo-list.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
